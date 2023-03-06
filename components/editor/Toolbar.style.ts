eba2e9a44c236a3a2d930dd1afca498aceb66a1f
J1min <aoiharu41@gmail.com>
2023-03-06 14:55:41 +0900

    Feat: 툴바 레이아웃 추가

---
+++ components/editor/Toolbar.style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ToolbarWrapper = styled.div`
+  padding: 1rem;
+  display: flex;
+  gap: 10px;
+`;
+
+export const ToolbarIcon = styled.div`
+  background-color: black;
+  color: white;
+  padding: 0.3rem;
+`;
---
+++ components/editor/Toolbar.tsx
@@ -0,0 +1,12 @@
+import * as S from "./Toolbar.style";
+
+export default function Toolbar() {
+  return (
+    <S.ToolbarWrapper>
+      <S.ToolbarIcon>이미지</S.ToolbarIcon>
+      <S.ToolbarIcon>단축어</S.ToolbarIcon>
+      <S.ToolbarIcon>단</S.ToolbarIcon>
+      <S.ToolbarIcon>단</S.ToolbarIcon>
+    </S.ToolbarWrapper>
+  );
+}
