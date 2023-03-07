481c2d2dd3a603776b314edda691626461d5fce6
J1min <aoiharu41@gmail.com>
2023-03-07 20:24:36 +0900

    Feat: add tsx icons

---
+++ components/icons/editor/H1Icon.tsx
@@ -0,0 +1,19 @@
+export default function H1Icon() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 24H24M8 36V12M24 36V12M34 24L40 20V36"
+        stroke="#EDE5D8"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
