58d44fa1c669645f4aa0ad83d687765ade1bccdd
J1min <aoiharu41@gmail.com>
2023-03-09 22:42:04 +0900

    Feat: add detail layout

--- components/artwork/ArtworkDetail.style.ts
+++ components/artwork/ArtworkDetail.style.ts
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
-export const ArtworkDetailWrapper = styled.div``;
-export const ArtworkTitle = styled.h1``;
+export const ArtworkDetailWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin-top: 30px;
+`;
+export const ArtworkTitle = styled.h1`
+  margin: 0;
+`;
 export const ArtworkWriterName = styled.h3`
   font-size: 20px;
 `;
@@ -10,4 +17,22 @@ export const ArtworkWriterWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+`;
+
+export const ArtworkContentWrapper = styled.div`
+  margin-top: 20px;
+`;
+export const ArtworkDateWrapper = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+export const ArtworkDate = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+
+  svg {
+    width: 28px;
+    height: 28px;
+  }
 `;
