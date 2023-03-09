9cff565c0f1d3a1b79b9e6c1d94a48e7c6f3b1e3
J1min <aoiharu41@gmail.com>
2023-03-09 17:55:05 +0900

    Feat: add submit button

--- components/login/LoginButton.style.ts
+++ components/login/LoginButton.style.ts
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import { Button } from "../atoms/Button.style";
 
-export const LoginButton = styled(Button)`
+export const LoginButton = styled(Button)<{ isFull?: boolean }>`
   font-family: CookieRun-Regular;
   padding: 12px 11rem;
+  width: ${(props) => (props.isFull ? "100%" : "")};
   border-radius: 9999px;
   font-size: 24px;
   display: flex;
+  justify-content: center;
   align-items: center;
   gap: 12px;
 `;
