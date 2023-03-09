6a435ceb044dc62d02f1a8caf057f30663af8e7c
J1min <aoiharu41@gmail.com>
2023-03-09 10:32:19 +0900

    Feat: add shadow

--- components/common/header/index.style.ts
+++ components/common/header/index.style.ts
@@ -7,4 +7,5 @@ export const HeaderWrapper = styled.header`
   align-items: center;
   justify-content: space-between;
   padding: 0 6rem;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 `;
