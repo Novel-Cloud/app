25c11b0ac37ee0baf69f47299b528aa2210fc987
J1min <aoiharu41@gmail.com>
2023-03-09 11:13:00 +0900

    Feat: add toolbar icom

--- components/editor/Toolbar.tsx
+++ components/editor/Toolbar.tsx
@@ -1,32 +1,53 @@
-import EditButton from "./EditButton";
+import AlignCenterIcon from "../icons/editor/AlignCenterIcon";
+import AlignLeftIcon from "../icons/editor/AlignLeftIcon";
+import AlignRightIcon from "../icons/editor/AlignRightIcon";
+import ParagraphIcon from "../icons/editor/ParagraphIcon";
+import ToolbarButtonView from "./ToolbarButton";
 import * as S from "./Toolbar.style";
+import UnOrderedListIcon from "../icons/editor/UnOrderedListIcon";
+import OrderedListIcon from "../icons/editor/OrderedListIcon";
+import ShortCutIcon from "../icons/editor/ShortCutIcon";
+import HrIcon from "../icons/editor/HrIcon";
+import ItalicIcon from "../icons/editor/ItalicIcon";
+import BoldIcon from "../icons/editor/BoldIcon";
+import ImageIcon from "../icons/editor/ImageIcon";
+import H5Icon from "../icons/editor/H5Icon";
+import H4Icon from "../icons/editor/H4Icon";
+import H3Icon from "../icons/editor/H3Icon";
+import H2Icon from "../icons/editor/H2Icon";
+import H1Icon from "../icons/editor/H1Icon";
 
 export default function ToolbarView() {
   const editButtonArgumentList = [
-    { cmd: "justifyCenter" },
-    { cmd: "justifyLeft" },
-    { cmd: "justifyRight" },
-    { cmd: "insertParagraph" },
-    { cmd: "insertUnorderedList" },
-    { cmd: "insertOrderedList" },
-    { cmd: "insertText", arg: "엄준식", name: "generate um" },
-    { cmd: "insertHorizontalRule" },
-    { cmd: "italic", arg: "i" },
-    { cmd: "bold", arg: "b" },
-    { cmd: "insertImage", arg: "https://placekitten.com/200/300" },
-    { cmd: "formatBlock", arg: "h1", name: "heading" },
-    { cmd: "fontSize", arg: "1", name: "h7" },
-    { cmd: "fontSize", arg: "2", name: "h6" },
-    { cmd: "fontSize", arg: "3", name: "h5" },
-    { cmd: "fontSize", arg: "4", name: "h4" },
-    { cmd: "fontSize", arg: "5", name: "h3" },
-    { cmd: "fontSize", arg: "6", name: "h2" },
-    { cmd: "fontSize", arg: "7", name: "h1" },
+    { cmd: "justifyCenter", icon: <AlignCenterIcon /> },
+    { cmd: "justifyLeft", icon: <AlignLeftIcon /> },
+    { cmd: "justifyRight", icon: <AlignRightIcon /> },
+    { cmd: "insertParagraph", icon: <ParagraphIcon /> },
+    { cmd: "insertUnorderedList", icon: <UnOrderedListIcon /> },
+    { cmd: "insertOrderedList", icon: <OrderedListIcon /> },
+    {
+      cmd: "insertText",
+      arg: "엄준식",
+      icon: <ShortCutIcon />,
+    },
+    { cmd: "insertHorizontalRule", icon: <HrIcon /> },
+    { cmd: "italic", arg: "i", icon: <ItalicIcon /> },
+    { cmd: "bold", arg: "b", icon: <BoldIcon /> },
+    {
+      cmd: "insertImage",
+      arg: "https://placekitten.com/200/300",
+      icon: <ImageIcon />,
+    },
+    { cmd: "fontSize", arg: "1", icon: <H5Icon /> },
+    { cmd: "fontSize", arg: "3", icon: <H4Icon /> },
+    { cmd: "fontSize", arg: "5", icon: <H3Icon /> },
+    { cmd: "fontSize", arg: "6", icon: <H2Icon /> },
+    { cmd: "fontSize", arg: "7", icon: <H1Icon /> },
   ];
   return (
     <S.ToolbarWrapper>
       {editButtonArgumentList.map((editButtonArgument, idx) => (
-        <EditButton key={idx} {...editButtonArgument} />
+        <ToolbarButtonView key={idx} {...editButtonArgument} />
       ))}
     </S.ToolbarWrapper>
   );
