ef78f9678502f41250ea7b09f00949b6662cee09
J1min <aoiharu41@gmail.com>
2023-03-10 10:40:57 +0900

    Test: getGreeting

--- components/common/header/SearchBar.tsx
+++ components/common/header/SearchBar.tsx
@@ -1,4 +1,5 @@
 import SearchIcon from "@/components/icons/editor/SearchIcon";
+import { getGreeting } from "@/utils/date";
 import { UseFormRegisterReturn } from "react-hook-form";
 import * as S from "./SearchBar.style";
 
@@ -11,7 +12,7 @@ export default function SearchBar({
 }) {
   return (
     <S.SearchBarWrapper>
-      <S.SearchBar placeholder="ohayo!" {...registerReturn} />
+      <S.SearchBar placeholder={getGreeting()} {...registerReturn} />
       <S.SearchIconWrapper onClick={onSearch}>
         <SearchIcon />
       </S.SearchIconWrapper>
