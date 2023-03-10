25dc95df0ef78a5bca13e477da193e52b91351d3
J1min <aoiharu41@gmail.com>
2023-03-11 01:38:00 +0900

    Feat: fix artwork thumbnail layout

--- components/artwork/ArtworkPlayer.style.ts
+++ components/artwork/ArtworkPlayer.style.ts
@@ -6,7 +6,9 @@ export const ArtworkPlayerWrapper = styled.div`
   height: 550px;
   margin: 0 250px;
   border: 1px solid black;
+  border-radius: 6px;
   img {
+    border-radius: 6px;
     object-fit: cover;
   }
 `;
