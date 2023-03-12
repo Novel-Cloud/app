aec3614f2b7afead09d75e292a6d7a5deb205101
J1min <aoiharu41@gmail.com>
2023-03-13 08:19:06 +0900

    Feat: 로그인 반응형 추가

--- components/login/LoginButton.style.ts
+++ components/login/LoginButton.style.ts
@@ -2,14 +2,17 @@ import styled from "styled-components";
 import { Button } from "../atoms/Button.style";
 
 export const LoginButton = styled(Button)<{ isFull?: boolean }>`
-  font-family: ${(props) => (props.isFull ? "" : "CookieRun-Regular")};
-  padding: 0.75rem 11rem;
-  width: ${(props) => (props.isFull ? "100%" : "")};
-  border-radius: 9999px;
-  font-size: 1.25rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  width: ${(props) => (props.isFull ? "100%" : "")};
+  padding: 0.75rem 11rem;
+  font-family: ${(props) => (props.isFull ? "" : "CookieRun-Regular")};
+  border-radius: 9999px;
+  font-size: 1.25rem;
   gap: 0.75rem;
   font-weight: 900;
+  @media screen and (max-width: 768px) {
+    padding: 0.75rem 3rem;
+  }
 `;
