97743eb8242af24e900ff3bbc7012f92a51a0697
J1min <aoiharu41@gmail.com>
2023-03-12 16:06:50 +0900

    Chore: add image style

--- components/artwork/ArtworkPlayer.style.ts
+++ components/artwork/ArtworkPlayer.style.ts
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const ArtworkPlayerWrapper = styled.div`
   margin: 0 15.625rem;
+  filter: drop-shadow(0px 0.25rem 0.25rem rgba(0, 0, 0, 0.25));
+  img {
+    border-radius: 0.375rem;
+  }
 `;
