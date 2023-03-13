dd959233be6f731e6b8411710131b451197b0cee
J1min <aoiharu41@gmail.com>
2023-03-13 20:36:58 +0900

    Feat: Export to pdf 버튼 추가

--- components/pdf/PdfService.tsx
+++ components/pdf/PdfService.tsx
@@ -4,12 +4,14 @@ import { PDFExport } from "@progress/kendo-react-pdf";
 import Button from "../atoms/Button";
 
 const PDFServiceWrapper = styled.div`
+  padding: 20px;
   * {
     font-family: "Pretendard-Variable" !important;
   }
   @font-face {
     font-family: "Pretendard-Variable";
     src: url("/assets/font/PretendardVariable.ttf") format("truetype");
+    font-weight: 700;
   }
 `;
 
@@ -23,17 +25,17 @@ export default function PDFService({ children }: { children: ReactNode }) {
 
   return (
     <PDFServiceWrapper>
-      <Button onClick={exportPDFWithMethod}>Export with method</Button>
       <PDFExport
         ref={pdfExportComponent}
         paperSize="a4"
-        fileName={`엄준식 Report for ${new Date().getFullYear()}`}
+        fileName="이현준을 향한 나의 사랑"
         author="엄준식"
         date={new Date()}
         title="엄준식의 모험"
       >
         <div id="pdf">{children}</div>
       </PDFExport>
+      <Button onClick={exportPDFWithMethod}>Export to pdf</Button>
     </PDFServiceWrapper>
   );
 }
