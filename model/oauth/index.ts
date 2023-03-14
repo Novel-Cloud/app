ef56acbe90eb84dbf24545c28d178afeca0708a4
J1min <aoiharu41@gmail.com>
2023-03-14 12:14:44 +0900

    Feat: Token interface 추가

--- model/oauth/index.ts
+++ model/oauth/index.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import httpClient from "@/apis";
 import KEY from "@/key";
+import { Token } from "@/types/user.interface";
 
 const useOauth = (platform: "google", authCode: string) => {
-  const { data } = useQuery(
+  const { data } = useQuery<Token>(
     [KEY.OAUTH, authCode],
     () =>
       httpClient.oauth
