dfe7f00a69f26d31ecfbc9f25f4634925edb12a2
J1min <aoiharu41@gmail.com>
2023-04-06 17:54:32 +0900

    refactor: 단축어 레이아웃 리팩토링

--- components/editor/edit/EditorEditApp.tsx
+++ components/editor/edit/EditorEditApp.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useShortCutList } from "@/model/editor";
+import { useQueryClient } from "@tanstack/react-query";
 import { ShortCut } from "@/types/editor.interface";
 import httpClient from "@/apis";
+import KEY from "@/key";
 import DragDropView from "./DragDropView";
 
 export default function EditorEditApp() {
+  const queryClient = useQueryClient();
   const { data: shortCutList, isError } = useShortCutList();
   const [myShortCutList, setMyShortCutList] = useState<ShortCut[]>([]);
   const [isChanged, setIsChanged] = useState(false);
@@ -18,7 +21,9 @@ export default function EditorEditApp() {
       const shortcutIdList = myShortCutList.map(
         (myShortCut) => myShortCut.shortcutId,
       );
-      httpClient.shortcut.sequence({ shortcutIdList });
+      httpClient.shortcut.sequence({ shortcutIdList }).then(() => {
+        queryClient.invalidateQueries([KEY.SHORTCUTLIST]);
+      });
     }
   }, [isChanged, myShortCutList]);
 
