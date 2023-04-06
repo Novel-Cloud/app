d30743487fa58d8aa9545f7d7eef82ec72760b18
J1min <aoiharu41@gmail.com>
2023-04-07 08:50:37 +0900

    feat: artwork ssr 추가

--- pages/artwork/[artworkId].tsx
+++ pages/artwork/[artworkId].tsx
@@ -1,20 +1,57 @@
+import httpClient from "@/apis";
 import ArtworkComment from "@/components/artwork/ArtworkCommentList";
 import ArtworkDetail from "@/components/artwork/ArtworkDetail";
 import ArtworkPlayer from "@/components/artwork/ArtworkPlayer";
 import ArtworkDetailLayout from "@/layout/ArtworkDetailLayout";
-import { useArtwork } from "@/model/artwork";
+import { Artwork } from "@/types/artwork.interface";
+import { deepcopy } from "@/utils/array";
+import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
 
-export default function ArtworkDetailPage() {
+interface ArtworkDetailPageProps {
+  artwork: Artwork;
+}
+
+export default function ArtworkDetailPage({ artwork }: ArtworkDetailPageProps) {
   const router = useRouter();
-  const { artworkId } = router.query;
-  const { data: artwork } = useArtwork(Number(artworkId));
+  console.log(artwork);
+
+  // const { data: artwork } = useArtwork(Number(artworkId));
 
   return (
-    <ArtworkDetailLayout
-      app={<ArtworkPlayer artwork={artwork} />}
-      detail={<ArtworkDetail artwork={artwork} />}
-      comment={<ArtworkComment artwork={artwork} />}
-    />
+    <>d</>
+    // <ArtworkDetailLayout
+    //   app={<ArtworkPlayer artwork={artwork} />}
+    //   detail={<ArtworkDetail artwork={artwork} />}
+    //   comment={<ArtworkComment artwork={artwork} />}
+    // />
   );
 }
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const artworkId = context.params?.artworkId || 0;
+  const { data: artwork } = await httpClient.artworkDetail
+    .getById({
+      params: { id: artworkId },
+    })
+    .then((r) => ({ data: deepcopy(r.data) }))
+    .catch((e) => {
+      console.log(e);
+
+      return { data: {} };
+    });
+
+  return {
+    props: {
+      artwork,
+    },
+    revalidate: 6000,
+  };
+};
