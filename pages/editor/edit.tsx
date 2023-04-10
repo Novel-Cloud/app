bb9a285f018f4b676d16967f3da2dd2e5d60e58d
J1min <aoiharu41@gmail.com>
2023-04-10 19:15:53 +0900

    feat(shortcut): 단축어 레이아웃 추가

--- pages/editor/edit.tsx
+++ pages/editor/edit.tsx
@@ -1,6 +1,9 @@
+import CreateShortCut from "@/components/editor/edit/CreateShortCut";
 import EditorEditApp from "@/components/editor/edit/EditorEditApp";
 import EditorEditLayout from "@/layout/EditorEditLayout";
 
 export default function Edit() {
-  return <EditorEditLayout app={<EditorEditApp />} />;
+  return (
+    <EditorEditLayout app={<EditorEditApp />} input={<CreateShortCut />} />
+  );
 }
