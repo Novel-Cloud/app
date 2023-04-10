18375106e83ac40f4ca4b150ffcaf40a9f5356c0
J1min <aoiharu41@gmail.com>
2023-04-10 18:55:06 +0900

    feat(user): image httpClient 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
       { protocol: "https", hostname: "velog.velcdn.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "http", hostname: "10.150.151.237" },
+      { protocol: "http", hostname: "localhost" },
     ],
   },
 };
