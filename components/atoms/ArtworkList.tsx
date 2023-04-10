fe21cb71bdb1d8884b1e339980137b0421824490
J1min <aoiharu41@gmail.com>
2023-04-10 09:22:43 +0900

    feat(artwork): 무한스크롤 기능 추가

--- components/atoms/ArtworkList.tsx
+++ components/atoms/ArtworkList.tsx
@@ -1,7 +1,12 @@
+import { useInView } from "react-intersection-observer";
+import styled from "styled-components";
+import { useEffect } from "react";
+import { FetchNextPageType } from "@/types/common.interface";
 import { Artwork } from "@/types/artwork.interface";
 import ButtonList from "../app/ButtonList";
 import ArtworkView from "./Artwork";
 import * as S from "./Artwork.style";
+import Loading from "./Loading";
 
 interface ArtworkPages {
   list: Artwork[];
@@ -9,13 +14,26 @@ interface ArtworkPages {
 
 interface ArtworkListProps {
   artworkPages: ArtworkPages[];
+  customHasNextPage: boolean;
+  fetchNextPage: FetchNextPageType<ArtworkPages>;
+  isFetchingNextPage: boolean;
   isButtonList?: boolean;
 }
 
 export default function ArtworkList({
   artworkPages,
+  customHasNextPage,
+  fetchNextPage,
+  isFetchingNextPage,
   isButtonList = false,
 }: ArtworkListProps) {
+  const { ref, inView } = useInView();
+  useEffect(() => {
+    if (inView && customHasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, customHasNextPage]);
+
   return (
     <S.ArtworkFrameWrapper>
       {isButtonList && <ButtonList />}
@@ -26,6 +44,13 @@ export default function ArtworkList({
           )),
         )}
       </S.ArtworkListWrapper>
+      <InfiniteScrollDiv ref={ref}>
+        {isFetchingNextPage && <Loading />}
+      </InfiniteScrollDiv>
     </S.ArtworkFrameWrapper>
   );
 }
+
+const InfiniteScrollDiv = styled.div`
+  height: 100px;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,24 @@ import React from "react";
 
 export default function Main() {
   const { data: tagList } = useTag();
-  const { pages: artworkPages } = useArtworkList();
+  const {
+    pages: artworkPages,
+    customHasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useArtworkList();
   return (
     <MainLayout
       tags={<TagList tagList={tagList} />}
-      app={<ArtworkList artworkPages={artworkPages} isButtonList />}
+      app={
+        <ArtworkList
+          artworkPages={artworkPages}
+          customHasNextPage={customHasNextPage}
+          fetchNextPage={fetchNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+          isButtonList
+        />
+      }
     />
   );
 }
