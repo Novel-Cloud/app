5581538c62e193a77ab2f0c5ad28ca4e044824e4
J1min <aoiharu41@gmail.com>
2023-04-11 20:17:44 +0900

    feat(comment): comment key 추가

--- key/index.ts
+++ key/index.ts
@@ -1,9 +1,21 @@
 const OAUTH = "useOauth";
 const USER = "useAuthUser";
+const MEMBER = "useMember";
 const SHORTCUTLIST = "useShortCutList";
 const CONTENT = "useContent";
 const ARTWORKLIST = "useArtworkList";
 const TAG = "useTag";
 const ARTWORK = "useArtwork";
-const KEY = { OAUTH, USER, SHORTCUTLIST, CONTENT, ARTWORKLIST, ARTWORK, TAG };
+const COMMENT = "useComment";
+const KEY = {
+  OAUTH,
+  USER,
+  MEMBER,
+  SHORTCUTLIST,
+  CONTENT,
+  ARTWORKLIST,
+  ARTWORK,
+  TAG,
+  COMMENT,
+};
 export default KEY;
