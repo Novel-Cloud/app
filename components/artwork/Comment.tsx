3159c05eb01e1a7e2b14140c36a8f24e016d628e
J1min <aoiharu41@gmail.com>
2023-04-11 20:17:09 +0900

    fix(comment): 댓글, 대댓글 레이아웃 추가

--- components/artwork/Comment.tsx
+++ components/artwork/Comment.tsx
@@ -1,8 +1,10 @@
 import { Artwork, Comment } from "@/types/artwork.interface";
-import Image from "next/image";
+import styled from "styled-components";
 import { useReducer } from "react";
 import Input from "../atoms/Input";
 import * as S from "./ArtworkCommentList.style";
+import Button from "../atoms/Button";
+import Avatar from "../atoms/Avatar";
 
 export default function CommentView({
   artwork,
@@ -18,22 +20,22 @@ export default function CommentView({
 
   return (
     <S.Comment>
-      <Image
-        src={artwork.writer.picture}
-        alt={artwork.writer.nickname}
+      <Avatar
+        src={comment.writer.picture}
+        alt={comment.writer.nickname}
         width={48}
         height={48}
         onClick={moveToProfile}
       />
       <div>
         <S.CommentWriter onClick={moveToProfile}>
-          {artwork.writer.nickname}
+          {comment.writer.nickname}
         </S.CommentWriter>
         <S.CommentContent>{comment.content}</S.CommentContent>
         <S.CommentDate>2022. 2. 3.</S.CommentDate>
-        <span onClick={() => toggleInputOpen()}>답글</span>
+        <span onClick={toggleInputOpen}>답글 열기</span>
         {comment.replyList.length !== 0 && (
-          <span onClick={() => toggleReplyOpen()}>열기</span>
+          <span onClick={toggleReplyOpen}>열기</span>
         )}
 
         {isReplyOpen && (
@@ -48,8 +50,19 @@ export default function CommentView({
           </>
         )}
 
-        {isInputOpen && <Input varient="secondary" isFull />}
+        {isInputOpen && (
+          <CommentInputWrapper>
+            <Input varient="secondary" isFull />
+            <Button>작성</Button>
+          </CommentInputWrapper>
+        )}
       </div>
     </S.Comment>
   );
 }
+
+const CommentInputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
