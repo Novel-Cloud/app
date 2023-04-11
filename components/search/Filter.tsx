67eb9b9eacd7c68771a2a93ccca12caf6cb6f12f
J1min <aoiharu41@gmail.com>
2023-04-11 12:16:03 +0900

    feat(search): filter 추가

---
+++ components/search/Filter.tsx
@@ -0,0 +1,158 @@
+import styled from "styled-components";
+import { Dispatch, SetStateAction, useReducer, useState } from "react";
+import { Filter } from "@/model/artwork";
+import FilterIcon from "../icons/common/FilterIcon";
+import CheckBoxText from "../atoms/CheckBoxText";
+
+interface FilterViewProps {
+  filter: Filter;
+  setFilter: Dispatch<SetStateAction<Filter>>;
+}
+
+const enum PropertyEnum {
+  DATE_AN_HOUR_AGO = "AN_HOUR_AGO",
+  DATE_TODAY = "TODAY",
+  DATE_THIS_WEEK = "THIS_WEEK",
+  DATE_THIS_MONTH = "THIS_MONTH",
+  DATE_THIS_YEAR = "THIS_YEAR",
+  ARTWORK_NOVEL = "NOVEL",
+  ARTWORK_ILLUSTRATION = "ILLUSTRATION",
+  SORT_UPLOAD_DATE = "UPLOAD_DATE",
+  SORT_VIEWS = "VIEWS",
+  SORT_LIKES = "LIKES",
+}
+
+export default function FilterView({ filter, setFilter }: FilterViewProps) {
+  const [isOpen, toggleOpen] = useReducer((state) => !state, false);
+  const [uploadDatePropertyId, setUploadDatePropertyId] = useState("");
+  const [theme, setTheme] = useState("");
+  const [sortTypePropertyId, setSortTypePropertyId] = useState("");
+
+  const datePropertyList = [
+    {
+      id: PropertyEnum.DATE_AN_HOUR_AGO,
+      label: "지난 1시간",
+      value: PropertyEnum.DATE_AN_HOUR_AGO,
+    },
+    {
+      id: PropertyEnum.DATE_TODAY,
+      label: "오늘",
+      value: PropertyEnum.DATE_TODAY,
+    },
+    {
+      id: PropertyEnum.DATE_THIS_WEEK,
+      label: "이번 주",
+      value: PropertyEnum.DATE_THIS_WEEK,
+    },
+    {
+      id: PropertyEnum.DATE_THIS_MONTH,
+      label: "이번 달",
+      value: PropertyEnum.DATE_THIS_MONTH,
+    },
+    {
+      id: PropertyEnum.DATE_THIS_YEAR,
+      label: "올해",
+      value: PropertyEnum.DATE_THIS_YEAR,
+    },
+  ];
+
+  const sortTypePropertyList = [
+    {
+      id: PropertyEnum.SORT_UPLOAD_DATE,
+      label: "업로드순",
+      value: PropertyEnum.SORT_UPLOAD_DATE,
+    },
+    {
+      id: PropertyEnum.SORT_LIKES,
+      label: "좋아요순",
+      value: PropertyEnum.SORT_LIKES,
+    },
+    {
+      id: PropertyEnum.SORT_VIEWS,
+      label: "조회수순",
+      value: PropertyEnum.SORT_VIEWS,
+    },
+  ];
+
+  const themeList = [
+    {
+      id: PropertyEnum.ARTWORK_ILLUSTRATION,
+      label: "그림",
+      value: PropertyEnum.ARTWORK_ILLUSTRATION,
+    },
+    {
+      id: PropertyEnum.ARTWORK_NOVEL,
+      label: "소설",
+      value: PropertyEnum.ARTWORK_NOVEL,
+    },
+  ];
+
+  return (
+    <>
+      <FilterWrapper onClick={toggleOpen}>
+        <FilterIcon />
+        필터
+      </FilterWrapper>
+
+      {isOpen && (
+        <div>
+          <div className="flex gap-32 my-8">
+            <div className="flex flex-col">
+              <h2 className="font-semibold">업로드 날짜</h2>
+              <hr className="my-2.5 text-white w-40" />
+              {datePropertyList.map(({ id, label, value }) => (
+                <CheckBoxText
+                  name="uploadDateType"
+                  key={id}
+                  id={id}
+                  label={label}
+                  value={value}
+                  filter={filter}
+                  setFilter={setFilter}
+                  checkBoxPropertyId={uploadDatePropertyId}
+                  setCheckBoxPropertyId={setUploadDatePropertyId}
+                />
+              ))}
+            </div>
+            <div>
+              <h2 className="font-semibold">정렬기준</h2>
+              <hr className="my-2.5 text-white w-40" />
+              {sortTypePropertyList.map(({ id, label, value }) => (
+                <CheckBoxText
+                  name="sortType"
+                  key={id}
+                  id={id}
+                  label={label}
+                  value={value}
+                  filter={filter}
+                  setFilter={setFilter}
+                  checkBoxPropertyId={sortTypePropertyId}
+                  setCheckBoxPropertyId={setSortTypePropertyId}
+                />
+              ))}
+            </div>
+            <div>
+              <h2 className="font-semibold">장르</h2>
+              <hr className="my-2.5 text-white w-40" />
+              {themeList.map(({ id, label, value }) => (
+                <CheckBoxText
+                  name="artworkType"
+                  key={id}
+                  id={id}
+                  label={label}
+                  value={value}
+                  filter={filter}
+                  setFilter={setFilter}
+                  checkBoxPropertyId={theme}
+                  setCheckBoxPropertyId={setTheme}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+const FilterWrapper = styled.div``;
