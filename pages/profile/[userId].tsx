83db05c5623055fbe6b48e9e92faf935ad9883c0
J1min <aoiharu41@gmail.com>
2023-04-11 20:19:35 +0900

    fix(user): 마이페이지 판별 로직 변경

--- pages/profile/[userId].tsx
+++ pages/profile/[userId].tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/common/Header";
 import ProfileBackground from "@/components/profile/ProfileBackground";
 import ProfileInfo from "@/components/profile/ProfileInfo";
+import useAuthUser from "@/hooks/useAuthUser";
 import ProfileLayout from "@/layout/ProfileLayout";
 import { useMember } from "@/model/user";
 import { useRouter } from "next/router";
@@ -9,7 +10,8 @@ export default function ArtworkDetailPage() {
   const router = useRouter();
   const { userId } = router.query;
   const { data: userInfo } = useMember(Number(userId));
-  const isMypage = Number(userId) === userInfo.memberId;
+  const { user: myUserInfo } = useAuthUser();
+  const isMypage = Number(userId) === myUserInfo.memberId;
 
   return (
     <>
