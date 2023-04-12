043b36369668c42a05906748b2ce2778e3197bb4
J1min <aoiharu41@gmail.com>
2023-04-12 20:31:05 +0900

    fix(comment): 케밥 버튼 자잘한 스타일 수정

--- components/atoms/Kebab.tsx
+++ components/atoms/Kebab.tsx
@@ -23,6 +23,7 @@ const ProviderWrapper = styled.div`
   position: relative;
   cursor: pointer;
   user-select: none;
+  color: ${theme.primary};
   svg {
     &:hover {
       opacity: 0.5;
@@ -36,10 +37,21 @@ function Menu({ children }: HTMLProps<HTMLUListElement>) {
 
 const MenuWrapper = styled.ul`
   position: absolute;
-  top: 24px;
-  right: 0;
+  top: 0px;
+  left: 8px;
+  margin: 0;
   box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
   z-index: 50;
+  padding: 0;
+  border-radius: 10px;
+  & > li:first-child {
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
+  & > li:last-child {
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+  }
 `;
 
 function Item({ children, onClick }: HTMLProps<HTMLLIElement>) {
@@ -56,9 +68,9 @@ const ItemWrapper = styled.li`
   cursor: pointer;
   background-color: white;
   padding: 10px 0.3125rem;
-
+  list-style: none;
   &:hover {
-    background-color: ${theme.primary};
+    background-color: ${theme.secondary};
   }
 
   & > span {
