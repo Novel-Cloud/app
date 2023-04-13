d913930205d755d82be34ad3bbcce1ef34333db6
J1min <aoiharu41@gmail.com>
2023-04-13 23:04:12 +0900

    fix(pwa): config 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const withPlugins = require("next-compose-plugins");
+
 const withPWA = require("next-pwa")({
   dest: "public",
 });
 
-module.exports = withPWA({
+const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "velog.velcdn.com" },
@@ -11,4 +13,19 @@ module.exports = withPWA({
       { protocol: "http", hostname: "localhost" },
     ],
   },
-});
+};
+
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+        },
+      },
+    ],
+    // 추가 플러그인 작성
+  ],
+  nextConfig,
+);
