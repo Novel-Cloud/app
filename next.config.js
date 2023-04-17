a2e5ea0d52cb42be4f603f750361ba3583d3d61b
J1min <aoiharu41@gmail.com>
2023-04-17 09:55:01 +0900

    fix(image): 이미지 주소 config 추가

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
       { protocol: "https", hostname: "velog.velcdn.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "http", hostname: "10.150.151.237" },
-      { protocol: "http", hostname: "localhost" },
+      { protocol: "http", hostname: "ec2-3-34-192-167.ap-northeast-2.compute.amazonaws.com" },
     ],
   },
 };
