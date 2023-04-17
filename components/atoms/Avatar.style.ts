a71be2d187209e7e2b5995355ce93dd417758379
J1min <aoiharu41@gmail.com>
2023-04-17 17:19:13 +0900

    fix(server): aws s3 연동

--- components/atoms/Avatar.style.ts
+++ components/atoms/Avatar.style.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const AvatarWrapper = styled.div`
   img {
     border-radius: 50%;
+    object-fit: cover;
   }
 `;
--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "http", hostname: "10.150.151.237" },
       { protocol: "http", hostname: "ec2-3-34-192-167.ap-northeast-2.compute.amazonaws.com" },
+      { protocol: "https", hostname: "novel-cloud-bucket.s3.ap-northeast-2.amazonaws.com" },
     ],
   },
 };
